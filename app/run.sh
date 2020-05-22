cb62cc4e7cd32b5c0a53f2d9c0f39ff46c07dbf3
vasko <vaskovasilev94@yahoo.com>
2020-05-22 19:47:04 +0300

    Cleanup.

--- app/run.sh
+++ app/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-export mongo_host="mongo.netlyt.io"
-export mongo_user="vasko"
-export mongo_db="netvoid"
+export mongo_host="host"
+export mongo_user="user"
+export mongo_db="db"
 export mongo_port="27017"
-export mongo_pass="Y8Iwb6lI4gRdA+tbsaBtVj0sIRVuUedCOJfNyD4hymuRqG4WVNlY9BfQzZixm763"
+export mongo_pass="mpas"
